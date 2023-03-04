e819fb029ab1c2c884f5a8410a1a1cf2ba0e30d7
Rasul <rasul.zeynalov.95@gmail.com>
2023-03-04 21:14:38 +0400

    first files uploaded

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+
+const initialState = {
+  todos: [],
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+        ),
+      };
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+export default store;
